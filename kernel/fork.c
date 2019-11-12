fc620c89d7f7e0b713f9c8fc04441ad84e0d37f6
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-11-12 21:44:59 +0530

    kernel: do powerhal boost for zygote forks

--- kernel/fork.c
+++ kernel/fork.c
@@ -82,6 +82,7 @@
 #include <linux/simple_lmk.h>
 #include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
+#include <linux/power_hal.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -1803,6 +1804,7 @@ long _do_fork(unsigned long clone_flags,
 	if (task_is_zygote(current)) {
 		cpu_input_boost_kick_max(50);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
+		powerhal_boost_kick_max(50);
 	}
 
 	/*
